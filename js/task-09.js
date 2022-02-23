de30d615489a404db4fa8a681ac74ab76aa7034d
Oleksandr <81925784+NOSOV-ksa@users.noreply.github.com>
2022-02-24 01:10:03 +0200

    1

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+};
+
+function onActionBtn() {
+  const getRandomValue = getRandomHexColor;
+  refs.valueColor.textContent = getRandomValue();
+  refs.backgroundColor.style.backgroundColor = getRandomValue();
 }
 
 
 const refs = {
   valueColor: document.querySelector('.color'),
   backgroundColor: document.querySelector('body'),
-  btnChangeColor: document.querySelector('.cange-color')
+  btnChangeColor: document.querySelector('.change-color')
 };
 
-refs.btnChangeColor.addEventListener('click', getRandomHexColor)
+refs.btnChangeColor.addEventListener('click', onActionBtn)
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const refs = {
+  creatBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  input: document.querySelector('input'),
+  boxes: document.getElementById('boxes')
+};
+
+let firstSizeBox = 30;
+
+refs.boxes.style.width = `${firstSizeBox}px`;
+refs.boxes.style.height = `${firstSizeBox}px`;
+refs.boxes.style.backgroundcolor = `${getRandomColor()}px`
